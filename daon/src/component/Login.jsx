97bf44f7ec40943ef4637a6a7256b13ed33175a7
CJW <qscft8000@gmail.com>
2025-06-24 17:29:27 +0900

    로그인밑에추가

--- daon/src/component/Login.jsx
+++ daon/src/component/Login.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+const nav = useNavigate();
+
   return (
     <div className="join-container">
       <div className="join-title">로그인</div>
@@ -13,6 +17,16 @@ const Login = () => {
         <input type="password" placeholder="비밀번호" className="join-input" />
        <br></br>
         <button type="submit" className="join-button">로그인</button>
+      <div style={{ marginTop: '30px', textAlign: 'center', fontSize: '14px' }}>
+    <span style={{ color: '#999', 
+        cursor: 'pointer',
+        marginRight : '8px',
+        marginLeft : '-10px' }}>아이디 찾기</span>
+    <span style={{ color: '#999' }}> | </span>
+    <span style={{ color: '#999', cursor: 'pointer',marginLeft :'8px' }}>비밀번호 찾기</span>
+    <span style={{ color: '#999',marginLeft :'8px' }}> | </span>
+    <span style={{ color: '#999', cursor: 'pointer',marginLeft :'8px' }} onClick={()=>{nav('/join')}}>회원가입</span>
+  </div>
       </form>
     </div>
   );
