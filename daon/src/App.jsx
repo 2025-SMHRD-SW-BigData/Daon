7cc77165993aad1d5735abae16a9b6713eedf754
LHN <haneul3691@gmail.com>
2025-06-26 17:59:50 +0900

    커뮤니티 페이지 지역선택 및 필터 기능 구현

--- daon/src/App.jsx
+++ daon/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
-import {Route, Routes} from 'react-router-dom'
-import Main from './component/Main'
-import Map from './component/Map'
-import Join from './component/Join'
-import Login from './component/Login'
-import Chatbot from './component/Chatbot'
-import Map2 from './component/Map2'
-import MyPage from './component/MyPage'
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+// 페이지 컴포넌트 임포트
+import Main from './component/Main';
+import Map from './component/Map';
+import Map2 from './component/Map2';
+import Join from './component/Join';
+import Login from './component/Login';
+import Chatbot from './component/Chatbot';
+import MyPage from './component/MyPage';
+import CommunityPage from './component/CommunityPage';
+import PostDetail from './component/PostDetail';
+
 import './App.css';
 
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Routes>
-      <Route path='/' element={<Main></Main>}></Route>
-      <Route path='/map' element={<Map2></Map2>}></Route>
-      <Route path='/join' element={<Join></Join>}></Route>
-      <Route path='/login' element={<Login></Login>}></Route>
-      <Route path='/chatbot' element={<Chatbot></Chatbot>}></Route>
-      <Route path='/mypage' element={<MyPage></MyPage>}></Route>
-      
-
-
+      <Route path='/' element={<Main />} />
+      <Route path='/map' element={<Map2 />} />
+      <Route path='/join' element={<Join />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/chatbot' element={<Chatbot />} />
+      <Route path='/mypage' element={<MyPage />} />
+      <Route path='/community' element={<CommunityPage />} />
+      <Route path='/community/:id' element={<PostDetail />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
