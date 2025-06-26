883bc5a5faee4457b8294a7017c197803c31b688
LJW <dlwlsdn1203@gmail.com>
2025-06-26 16:43:20 +0900

    서버초안 커밋

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const userRouter = require('./routes/userRouter');
+const chatbotRouter = require('./routes/chatbotRouter');
+
+// CORS 미들웨어: 모든 도메인 허용 (개발용)
+app.use(cors());
+
+// POST 요청 바디 파싱 설정
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// 라우터 등록
+app.use(userRouter);
+app.use(chatbotRouter);
+
+// 서버 실행
+app.listen(3003, () => {
+  console.log('Server is running on port : 3003');
+});
