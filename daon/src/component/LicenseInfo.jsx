adbbca7e5d41dee883661324df4ae9527fbd58e2
LHN <haneul3691@gmail.com>
2025-07-01 12:47:02 +0900

    중간

--- daon/src/component/LicenseInfo.jsx
+++ daon/src/component/LicenseInfo.jsx
@@ -5,27 +5,26 @@ import NavBar from './NavBar';
 import '../style/licenseinfo.css';
 
 const LicenseInfo = () => {
-  const handleClick = (path) => {
-    window.location.href = path;
-  };
+    const handleClick = (path) => {
+        window.location.href = path;
+    };
 
-  return (
-    <div className="phon_size">
-      <Header />
+    return (
+        <div className="phon_size">
+            <Header />
 
-      <div className="license-container">
-        <h2 className="license-title">어업 자격</h2>
+            <div className="license-container">
+                <h2 className="license-title">어업 자격</h2>
 
-        <button className="license-btn" onClick={() => handleClick('/licensedfishery')}>면허어업</button>
-        <button className="license-btn" onClick={() => handleClick('/permitfishery')}>허가어업</button>
-        <button className="license-btn" onClick={() => handleClick('/reportfishery')}>신고어업</button>
-        <button className="license-btn" onClick={() => handleClick('/fishingboat')}>낚시어선업</button>
-        <button className="license-btn" onClick={() => handleClick('/fischool')}>자격증</button>
-      </div>
+                <button className="license-btn" onClick={() => handleClick('/licensedfishery')}>면허어업</button>
+                <button className="license-btn" onClick={() => handleClick('/permitfishery')}>허가어업</button>
+                <button className="license-btn" onClick={() => handleClick('/reportfishery')}>신고어업</button>
+                <button className="license-btn" onClick={() => handleClick('/fishingboat')}>낚시어선업</button>
+            </div>
 
-      <NavBar />
-    </div>
-  );
+            <NavBar />
+        </div>
+    );
 };
 
 export default LicenseInfo;
