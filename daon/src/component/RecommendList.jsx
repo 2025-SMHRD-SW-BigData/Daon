8fd5a2e23d1a5e2b5a49ec59cc7bf6d699474279
LHN <haneul3691@gmail.com>
2025-07-02 14:33:15 +0900

    중간저장

--- daon/src/component/RecommendList.jsx
+++ daon/src/component/RecommendList.jsx
@@ -1,27 +1,56 @@
-// src/component/RecommendList.jsx
+// src/component/PairTrawl.jsx
 import React from 'react';
-import { getRecommendedHarbors } from '../utils/recommendHarbors';
-import '../style/recommendlist.css'; // CSS 파일은 별도로 구성해주세요
+import Header from './Header';
+import NavBar from './NavBar';
+import RecommendList from './RecommendList';
+import '../style/pairtrawl.css';
 
-const RecommendList = ({ vesselType }) => {
-  const recommended = getRecommendedHarbors(vesselType);
-
+const PairTrawl = () => {
   return (
-    <div className="recommend-container">
-      <h3 className="recommend-title">🚢 관련 어항 목록 - {vesselType}</h3>
-      <ul className="recommend-list">
-        {recommended.length > 0 ? (
-          recommended.map((h, i) => (
-            <li key={i} className="recommend-item">
-              <strong>{h.name}</strong> ({h.region})
-            </li>
-          ))
-        ) : (
-          <li className="recommend-empty">해당 어선에 맞는 어항 정보가 없습니다.</li>
-        )}
-      </ul>
+    <div className="phon_size">
+      <Header />
+
+      <div className="scroll-area">
+        <div className="pairtrawl-container">
+          <h2 className="page-title">어선 정보</h2>
+          <h3 className="section-hash">권현망어선</h3>
+
+          <img
+            src="/UIimages/권현망어선.jpg"
+            alt="권현망어선"
+            className="pairtrawl-img"
+          />
+
+          <div className="pairtrawl-info">
+            <p>
+              <span className="blue bold">▼ 방식</span><br />
+              선단을 이루어 한 쌍의 어선이 그물 양쪽을 끌어당기며 조업
+            </p>
+            <p>
+              <span className="blue bold">▼ 주요 어획 어종</span><br />
+              멸치, 갈치, 전어 등
+            </p>
+            <p>
+              <span className="blue bold">▼ 작업 해역</span><br />
+              연근해 및 남해안 일대
+            </p>
+
+            {/* 추천 어항 리스트 삽입 */}
+            <RecommendList vesselType="권현망" />
+          </div>
+
+          <button
+            className="pairtrawl-btn"
+            onClick={() => window.open('http://www.ksupk.or.kr/file/index.php', '_blank')}
+          >
+            구매 관련 사이트 이동하기
+          </button>
+        </div>
+      </div>
+
+      <NavBar />
     </div>
   );
 };
 
-export default RecommendList;
+export default PairTrawl;
